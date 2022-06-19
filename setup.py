fef42718e646d654f32fdf6366c6374a08cb4525
fra <francesco.zuppichini@gmail.com>
2022-06-19 20:00:15 +0200

    added docs website generation

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as f:
+    long_description = f.read()
+
+with open("./requirements.txt", "r") as f:
+    install_requires = f.read().split("\n")
+
+setup(
+    name="glasses",
+    version="0.1.0",
+    author="Francesco Saverio Zuppichini & Anugunj Naman",
+    author_email="francesco.zuppichini@gmail.com",
+    description="Compact, concise and customizable deep learning computer vision.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FrancescoSaverioZuppichini/glasses",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=install_requires,
+    python_requires=">=3.9",
+    extras_require={"dev": ["pytest"]},
+)
